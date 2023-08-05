ed5dc1ebfa4fde992a8b2f9c183b2fcb961e6326
AryaDarisova <arisova.darya@yandex.ru>
2023-08-05 23:15:34 +0400

    Add storytelling variability functionality

--- destiny/Controller/ViewController.swift
+++ destiny/Controller/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Btn: UIButton!
+    @IBOutlet weak var choice2Btn: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        sender.layer.opacity = 0.5
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        if storyBrain.isEndOfStory {
+            let alert = UIAlertController(title: "Your story is over!", message: "Tap \"OK\" to reset storytelling", preferredStyle: UIAlertController.Style.alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+            storyBrain.resetStory()
+        }
+        
+        let currentStory = storyBrain.getCurrentStory()
+        
+        choice1Btn.layer.opacity = 1
+        choice2Btn.layer.opacity = 1
+        storyLabel.text = currentStory.title
+        choice1Btn.setTitle(currentStory.choice1, for: .normal)
+        choice2Btn.setTitle(currentStory.choice2, for: .normal)
+    }
 }
 
---
+++ destiny/Model/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  destiny
+//
+//  Created by Daria Arisova on 05.08.2023.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
